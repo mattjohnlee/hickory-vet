e1b600013a08e4af31a5783a81f0c9d3b78292f0
Matt Lee <matthewleedesign@gmail.com>
2015-02-07 11:51:58 -0500

    configure Pattern Lab based on Heron files
    Signed-off-by: Matt Lee <matthewleedesign@gmail.com>

--- styleguide/source/config.rb
+++ styleguide/source/config.rb
@@ -1,5 +1,8 @@
 require 'compass/import-once/activate'
 # Require any additional compass plugins here.
+require 'breakpoint' 
+require 'modular-scale' 
+
 
 # Set this to the root of your project when deployed:
 http_path = "/"
